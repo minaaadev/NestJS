7e446ee4f977b0fd772fef361cfcdd6c8fb6bfd4
minaaadev <minaaadev@gmail.com>
2025-03-09 22:09:58 +0900

    수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
 import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -26,7 +25,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
     }),
     UsersModule,  // UsersModule 연결
-    AuthModule,  // AuthModule 연결
+    
   ],
   controllers: [AppController],
   providers: [AppService],
